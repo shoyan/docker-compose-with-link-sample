78551fb185757bb2b77e86146e9375b6ddd61b8e
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-09-20 19:53:41 +0900

    Remove redundant option

--- login.sh
+++ login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v `pwd`:/app --net=dockercomposewithlinksample_default --link dockercomposewithlinksample_mysql_1:mysql dockercomposewithlinksample_web bash
+docker run --rm -it -v `pwd`:/app --net=dockercomposewithlinksample_default dockercomposewithlinksample_web bash
